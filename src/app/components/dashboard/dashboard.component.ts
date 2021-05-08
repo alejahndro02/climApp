03c77e741f139e93b4370b15f78dfae780759d00
Alejahndro <alejahndro02@gmail.com>
2021-05-08 14:13:38 -0500

    Se delcararon variables y tambien se declararon los metodos obtenerClima y error

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -9,13 +9,35 @@ import { ClimaService } from './../../services/clima.service';
 export class DashboardComponent implements OnInit {
  urlImg = 'https://image.flaticon.com/icons/png/512/1116/1116453.png';
  ciudad = ' ';
-  constructor(private _climaService: ClimaService) { }
+ temperatura = 0;
+ humedad = 0;
+ clima ='';
+ query = false;
+ loading = false;
+ mensajError= false;
+ constructor(private _climaService: ClimaService) { }
 
   ngOnInit(): void {
   }
   obtenerClima() {
+    this.query = false;
+    this.loading = true;
     this._climaService.getClima(this.ciudad).subscribe(data => {
-      console.log(data);
+      this.loading = false;
+      this.query = true;
+      this.temperatura = data.main.temp - 273
+      this.humedad = data.main.humidity
+      this.clima = data.weather[0].main
+    },error => {
+      this.loading = false;
+      this.error();
     })
+  };
+  error(){
+    this.mensajError =true;
+    setTimeout( () => {
+      this.mensajError =false;
+      this.ciudad='';
+    },3000);
   }
 }
